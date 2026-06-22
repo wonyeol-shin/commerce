8f3804456564ef1ddb6a0646c20b2ea6e73a5c94
wonyeolShin <lwy171400@gmail.com>
2026-06-22 16:14:32 +0900

    코드 중복부분 리펙토링

--- CommerceSystem.java
+++ CommerceSystem.java
@@ -32,34 +32,31 @@ public class CommerceSystem {
             System.out.flush();
             System.out.println("[ 실시간 커머스 플랫폼 메인 ]");
             String choice;
+            Integer indexNum;
+
             for (int i = 0; i < categories.size(); i++) {
                 System.out.println( (i + 1)  + "." + categories.get(i) );
             }
             System.out.println("0. 종료      | 프로그램 종료");
-
             choice = sc.nextLine();
+
+            if (isNotNum(choice)) {
+                continue;
+            }
+
             // 0 입력 시 종로
             if (choice.equals("0")) {
                 break;
             }
-            // 입력 값이 정수가 아닌경우
-            try {
-                Integer.parseInt(choice);
-            } catch (RuntimeException r) {
-                System.out.println("잘못된 값 입력");
-                continue;
-            }
 
+            indexNum = Integer.parseInt(choice) -1;
             // 없는 인덱스 범위라면
-            try {
-                 categories.get(Integer.parseInt(choice) -1);
-            } catch (IndexOutOfBoundsException index) {
-                System.out.println("없는 카테고리 입니다.");
+            if (isNotIndexBound(indexNum, categories)) {
                 continue;
             }
 
-//            // 입력한 인덱스의 카테고리 객체를 추출
-            Category selectedCategory = categories.get(Integer.parseInt(choice) -1);
+//           // 입력한 인덱스의 카테고리 객체를 추출
+            Category selectedCategory = categories.get(indexNum);
 
             // 입력받은 인덱스의 카테고리 이름과 해당 카테고리가 가지고 있는 상품들 추출
             System.out.println(selectedCategory.getCategory() + "카테고리");
@@ -72,33 +69,57 @@ public class CommerceSystem {
             System.out.println("0. 뒤로가기");
             choice = sc.nextLine();
 
+            if (isNotNum(choice)) {
+                continue;
+            }
+            // 0 입력시 처음 부분으로 복귀
             if (choice.equals("0")) {
                 continue;
             }
 
-            // 입력 값이 정수가 아닌경우
-            try {
-                Integer.parseInt(choice);
-            } catch (RuntimeException r) {
-                System.out.println("잘못된 값 입력");
+            indexNum = Integer.parseInt(choice) -1;
+            // 없는 상품 인덱스라면
+            if (isNotProductIndexBound(indexNum, products)) {
                 continue;
             }
 
-            try {
-                products.get(Integer.parseInt(choice) -1 );
-            } catch (IndexOutOfBoundsException u) {
-                System.out.println("없는 상품입니다.");
-                continue;
-            }
-
-            Product product = products.get(Integer.parseInt(choice) -1);
-
+            Product product = products.get(indexNum);
             System.out.println("선택한 상품: " + product + " | " + "재고: " + product.getProductStock() + "개");
 
         }
-
-
     }
+
+    private boolean isNotProductIndexBound(Integer choice, List<Product> products) {
+        try {
+            products.get(choice);
+        } catch (IndexOutOfBoundsException index) {
+            System.out.println("없는 상품입니다.");
+            return  true;
+        }
+        return false;
+    }
+
+    private boolean isNotIndexBound(Integer choice, List<Category> categories) {
+        try {
+            categories.get(choice);
+        } catch (IndexOutOfBoundsException index) {
+            System.out.println("없는 카테고리 입니다.");
+            return  true;
+        }
+        return false;
+    }
+
+
+    private boolean isNotNum(String choice) {
+        try {
+            Integer.parseInt(choice);
+        } catch (RuntimeException r) {
+            System.out.println("잘못된 값 입력");
+            return true;
+        }
+        return false;
+    }
+
     // 코드가 너무 길어지고 추후 기능 추가를 위해 카테고리와 상품 추가 분리
     private void commerceContext() {
         Category electronic =  addCategory("전자제품");
